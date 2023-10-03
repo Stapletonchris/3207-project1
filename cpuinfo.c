fbb5d3b14ceb15c09e5f7c342de663f0e48cb4c8
Christopher Christmas <cchristmas21@Christophers-MacBook-Pro.local>
2023-10-03 19:41:25 -0400

    added some changes

--- cpuinfo.c
+++ cpuinfo.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <dirent.h>
 #include <unistd.h>
 
 int main(int argc, char **argv)
 {
-
     int opt;
     char *path = NULL;
 
@@ -20,20 +18,20 @@ int main(int argc, char **argv)
             break;
         default:
             fprintf(stderr, "Usage: %s -f /proc/cpuinfo\n", argv[0]);
-            exit(0);
+            exit(1);
         }
     }
 
     if (path == NULL)
     {
         fprintf(stderr, "You must provide the -f option with /proc/cpuinfo\n");
-        exit(0);
+        exit(1);
     }
 
     if (strcmp(path, "/proc/cpuinfo") != 0)
     {
         fprintf(stderr, "Invalid file path. Use /proc/cpuinfo\n");
-        exit(0);
+        exit(1);
     }
 
     FILE *file = fopen(path, "r");
@@ -42,24 +40,41 @@ int main(int argc, char **argv)
         perror("Error opening /proc/cpuinfo");
         return 1;
     }
+
     char *line = NULL;
     size_t linecap = 0;
     ssize_t linelen;
-    while ((linelen = getline(&line, &linecap, fp)) > 0)
+
+    while ((linelen = getline(&line, &linecap, file)) != -1)
     {
+        // Find the "cache size" string in each line
+        char *cache_size = strstr(line, "cache size");
+
+        if (cache_size != NULL)
+        {
+            // Tokenize the line using ':' as the delimiter
+            char *token = strtok(line, ":");
+
+            // Find and print the cache size information
+            while (token != NULL)
+            {
+                token = strtok(NULL, ":");
+                if (token != NULL)
+                {
+                    printf("Cache Size: %s\n", token);
+                }
+            }
         }
-
-    // char line[300];
-    // while (fgets(line, sizeof(line), file))
-    // {
-    //     // Process each line in /proc/cpuinfo here
-    //     printf("%s", line);
-    // }
-
-    // If the file can be closed, close it
-    if ((fclose(file)) != 0)
-    {
-        perror("Error closing /proc/cpuinfo\n");
     }
+
+    // Close the file
+    fclose(file);
+
+    // Free allocated memory for getline
+    if (line != NULL)
+    {
+        free(line);
+    }
+
     return 0;
 }
